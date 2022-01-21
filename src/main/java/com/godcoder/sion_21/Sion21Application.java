a4468a4a96508a006148d5d2411ad7d1e285841b
rlatldhs32 <rlatldhs32@naver.com>
2022-01-21 11:50:59 +0900

    init

---
+++ src/main/java/com/godcoder/sion_21/Sion21Application.java
@@ -0,0 +1,13 @@
+package com.godcoder.sion_21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sion21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sion21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/godcoder/sion_21/Sion21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godcoder.sion_21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sion21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
