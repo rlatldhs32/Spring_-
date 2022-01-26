31ff743f327d7904f74b69df61ab59466a9c51a2
rlatldhs32 <rlatldhs32@naver.com>
2022-01-26 15:57:55 +0900

    220126 QueryDsl with gradel.

--- src/main/java/com/godcoder/sion_21/controller/UserApiController.java
+++ src/main/java/com/godcoder/sion_21/controller/UserApiController.java
@@ -2,8 +2,10 @@ package com.godcoder.sion_21.controller;
 
 
 import com.godcoder.sion_21.model.Board;
+import com.godcoder.sion_21.model.QUser;
 import com.godcoder.sion_21.model.User;
 import com.godcoder.sion_21.repository.UserRepository;
+import com.querydsl.core.types.Predicate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,12 +20,25 @@ public class UserApiController {
     @Autowired
     private UserRepository repository;
 
-    @GetMapping("/users")
-    List<User> all(){
-        List<User> users = repository.findAll();
-        log.debug("getBoards().size() 호출 전");
-        log.debug("getBoards().size() : {}",users.get(0).getBoards().size());
-        log.debug("getBoards().size() 호출 후");
+    @GetMapping("/users") //required=false 하면 안줘도 됨 만약 저거 안주면 무조건 파라미터 줘야함
+    List<User> all(@RequestParam(required = false) String method,@RequestParam(required = false) String text ){ //이 안에 매겝녀수하면 파라메터로 받음
+//        List<User> users = repository.findAll();
+        List<User> users = null;
+        if("query".equals(method)){
+            users=repository.findByUsernameQuery(text);
+        }else if("nativeQuery".equals(method)){
+            users=repository.findByUsernameNativeQuery(text);
+        }else if("querydsl".equals(method)){ //Q클래스명 -> 사용가능
+
+            QUser user = QUser.user;
+//            Predicate predicate = user.firstname.equalsIgnoreCase("dave") //대소문자 구분없이 dave라는 이름이면서
+//                    .and(user.lastname.startsWithIgnoreCase("mathews")); //last네임으 ㄴ저거
+//
+//            repository.findAll(predicate);
+        }
+        else{
+            users = repository.findAll();
+        }
         users.get(0).getBoards().size();
         return users; //사용자의 건수에 따라서 n+1 프라블럼.(너무 많이호출함. 따라서 )
     }
--- src/main/java/com/godcoder/sion_21/repository/UserRepository.java
+++ src/main/java/com/godcoder/sion_21/repository/UserRepository.java
@@ -4,13 +4,31 @@ import com.godcoder.sion_21.model.Board;
 import com.godcoder.sion_21.model.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long>, QuerydslPredicateExecutor<User> {
 
     @EntityGraph(attributePaths = {"boards"})
     List<User> findAll();
 
     User findByUsername(String username);
+
+    @Query("select u from User u where u.username like %?1%")
+    List<User> findByUsernameQuery(String firstname);
+
+    @Query(value = "select * from User u where u.username like %?1%",nativeQuery = true)
+    //JPQL이 아니라 순수 sql 쿼리가 됨. native Query써서.
+    List<User> findByUsernameNativeQuery(String firstname);
+
+    //마지막 저 뒤에꺼 extends해서, 아래와 같은 함수 사용 가능함.
+//    Optional<T> findById(Predicate predicate);
+//
+//    Iterable<T> findAll(Predicate predicate);
+//
+//    long count(Predicate predicate);
+//
+//    boolean exists(Predicate predicate);
 }
